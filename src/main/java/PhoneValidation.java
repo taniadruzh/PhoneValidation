d4561b7a4cef4dbdda75f0b3ca7b57d52629b483
taniadruzh <tania.druzhchenko@gmail.com>
2018-05-05 21:44:20 +0300

    com

--- src/main/java/PhoneValidation.java
+++ src/main/java/PhoneValidation.java
@@ -6,12 +6,18 @@ public class PhoneValidation {
 
     public PhoneValidation(String phoneNumber) {
 
-        this.phoneNumber = checkNumber(phoneNumber);
+        phoneNumber = prepareNumber(phoneNumber);
+        checkNumber(phoneNumber);
+
     }
 
-    private String checkNumber(String phoneNumber){
+    private String prepareNumber(String phoneNumber){
         phoneNumber = phoneNumber.replaceAll("\\D", "");
         phoneNumber = "+" + phoneNumber;
         return phoneNumber;
+    }
+
+    private String checkNumber(String phoneNumber) throws RuntimeException{
+        if(phoneNumber.contains("+0"))  throw new RuntimeException();
     }
 }
