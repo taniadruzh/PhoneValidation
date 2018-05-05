b68050e968808acc5b5d0bea9168f26e386f3496
taniadruzh <tania.druzhchenko@gmail.com>
2018-05-05 17:11:40 +0300

    com

--- src/main/java/PhoneValidation.java
+++ src/main/java/PhoneValidation.java
@@ -1,11 +1,17 @@
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.ToString;
 
-@Getter
-@Setter
-@AllArgsConstructor
+@ToString
 public class PhoneValidation {
+    private String phoneNumber;
 
+    public PhoneValidation(String phoneNumber) {
 
+        this.phoneNumber = checkNumber(phoneNumber);
+    }
+
+    private String checkNumber(String phoneNumber){
+        phoneNumber = phoneNumber.replaceAll("\\D", "");
+        phoneNumber = "+" + phoneNumber;
+        return phoneNumber;
+    }
 }
---
+++ src/main/java/Runner.java
@@ -0,0 +1,6 @@
+public class Runner {
+    public static void main(String[] args) {
+        PhoneValidation pV = new PhoneValidation("+38vf -(093D/7 4 7");
+        System.out.println(pV);
+    }
+}
